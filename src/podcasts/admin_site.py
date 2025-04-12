8dd6bbfb071aa6ae8f86458ae837202704ef28fa
Robert Huselius <robert@huseli.us>
2025-04-12 02:29:28 +0200

    delete challenge

--- src/podcasts/admin_site.py
+++ src/podcasts/admin_site.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.db.models import Q
+
 from podcasts.models.comment import Comment
 
 
--- src/podcasts/views/challenge.py
+++ src/podcasts/views/challenge.py
@@ -1,10 +1,14 @@
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.mixins import (
+    CreateModelMixin,
+    DestroyModelMixin,
+    RetrieveModelMixin,
+)
 from rest_framework.viewsets import GenericViewSet
 
 from podcasts import serializers
 from podcasts.models.challenge import Challenge
 
 
-class ChallengeViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+class ChallengeViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     serializer_class = serializers.ChallengeSerializer
     queryset = Challenge.objects.all()
