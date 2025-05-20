6052e01323f5835a55432e1b2b4fc9bd9828be9f
Robert Huselius <robert@huseli.us>
2025-05-20 02:04:48 +0200

    Maybe fixed TimestampField.to_python() returning str

--- src/utils/form_fields.py
+++ src/utils/form_fields.py
@@ -12,14 +12,17 @@ class TimestampField(forms.Field):
         return super().prepare_value(value)
 
     def to_python(self, value):
+        value = super().to_python(value)
+        if value in self.empty_values:
+            return None
         if isinstance(value, str) and value:
             value = value.replace(".", ":")
             parts = value.split(":")
             seconds = int(parts[-1]) if len(parts) > 0 else 0
             minutes = int(parts[-2]) if len(parts) > 1 else 0
             hours = int(parts[-3]) if len(parts) > 2 else 0
-            return seconds + (minutes * 60) + (hours * 60 * 60)
-        return super().to_python(value)
+            value = seconds + (minutes * 60) + (hours * 60 * 60)
+        return value
 
     def widget_attrs(self, widget):
         return {"class": "timestamp-field"}
