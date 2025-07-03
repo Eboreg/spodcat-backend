29b85f89307d14c4adb71e66a7ea667b1f8c5794
Robert Huselius <robert@huseli.us>
2025-07-04 00:30:58 +0200

    Did not get correct Last_modified for fontface view

--- src/spodcat/views/font_face.py
+++ src/spodcat/views/font_face.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from django.db.models import Max
 from django.http import HttpRequest, HttpResponse
 from django.utils.http import http_date
@@ -9,8 +7,7 @@ from spodcat.models import FontFace
 
 def font_face_css(request: HttpRequest):
     font_faces = FontFace.objects.all()
-    last_modified_dict = FontFace.objects.values(latest=Max("updated")).first()
-    last_modified: datetime | None = last_modified_dict["latest"] if last_modified_dict else None
+    last_modified = FontFace.objects.aggregate(latest=Max("updated"))["latest"]
     css = "\n".join(ff.get_css() for ff in font_faces).encode()
     headers = {
         "Content-Disposition": 'inline; filename="font-faces.css"',
